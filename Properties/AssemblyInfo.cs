7cd2144217585db99bdc126fbc6611212e95bdc1
Dimitar Grozev <dgrozev@ebay.com>
2021-11-12 10:31:21 -0800

    lastweek

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -20,8 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("a9a539c3-dc4d-44f1-9c7c-d6c40
-    a680a")]
+//[assembly: Guid("a9a539c3-dc4d-44f1-9c7c-d6c40a680a")]
 
 // Version information for an assembly consists of the following four values:
 //
